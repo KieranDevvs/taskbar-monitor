ccbf56ee9fdb35e3d142cfdc480849924488c09d
Kieran Devlin <kierandevvs@gmail.com>
2022-08-26 21:28:56 +0100

    Resources are now loaded from a relative path to the project.

--- TaskbarMonitorInstaller/Properties/Resources.Designer.cs
+++ TaskbarMonitorInstaller/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace TaskbarMonitorInstaller.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -81,7 +81,7 @@ namespace TaskbarMonitorInstaller.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 0.3.3.
+        ///   Looks up a localized string similar to 0.3.6.
         /// </summary>
         internal static string Version {
             get {
