faeb2ba35825a64b9ade486e20f0e5908c62a64e
Kieran Devlin <kierandevvs@gmail.com>
2022-08-26 21:00:06 +0100

    Updated MSBuild path.

--- batch/generaterelease.bat
+++ batch/generaterelease.bat
@@ -2,12 +2,12 @@
 
 echo Generating release files
 
-"%programfiles(x86)%\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\msbuild.exe"  /t:Rebuild /p:Configuration=Release /p:VisualStudioVersion=16.0 ..\TaskbarMonitor.sln
+"%programfiles%\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\msbuild.exe"  /t:Rebuild /p:Configuration=Release /p:VisualStudioVersion=16.0 ..\TaskbarMonitor.sln
 
 copy ..\TaskbarMonitor\bin\Release\TaskbarMonitor.dll ..\TaskbarMonitorInstaller\Resources
 copy ..\TaskbarMonitor\bin\Release\Newtonsoft.Json.dll ..\TaskbarMonitorInstaller\Resources
 
-"%programfiles(x86)%\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\msbuild.exe"  /t:Rebuild /p:Configuration=Release /p:VisualStudioVersion=16.0 ..\TaskbarMonitor.sln
+"%programfiles%\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\msbuild.exe"  /t:Rebuild /p:Configuration=Release /p:VisualStudioVersion=16.0 ..\TaskbarMonitor.sln
 
 copy ..\TaskbarMonitorInstaller\bin\Release\TaskbarMonitorInstaller.exe ..\build
 
